2bb132d1db18588348b40960890c656e8f4cc2ad
axlgutierrezl <axel.gutierrez@readinessit.com>
2026-07-29 11:28:09 -0500

    init demo

---
+++ src/main/java/com/axlgutierrezl26/demo_api_rest_java/DemoApiRestJavaApplication.java
@@ -0,0 +1,13 @@
+package com.axlgutierrezl26.demo_api_rest_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiRestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiRestJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axlgutierrezl26/demo_api_rest_java/DemoApiRestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axlgutierrezl26.demo_api_rest_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiRestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
